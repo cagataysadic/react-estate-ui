b01a6e9799432df7171303c9ff24d7357c983178
cagataysadic <ccagatays@hotmail.com>
2024-12-20 20:51:25 +0300

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,50 @@
+import HomePage from "./routes/homePage/homePage";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ListPage from "./routes/listPage/listPage";
+import Layout from "./routes/layout/layout";
+import Login from "./routes/login/login";
+import SinglePage from "./routes/singlePage/singlePage";
+import ProfilePage from "./routes/profilePage/profilePage";
+
+
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path:"/",
+          element: <HomePage />
+        },
+        {
+          path: "/list",
+          element: <ListPage />
+        },
+        {
+          path: "/login",
+          element: <Login />
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />
+        },
+        {
+          path: "/profile",
+          element: <ProfilePage />
+        },
+      ]
+    }
+  ]);
+
+
   return (
-    <div>Hello World</div>
-  )
+    <RouterProvider router={router}/>
+  );
 }
 
 export default App
